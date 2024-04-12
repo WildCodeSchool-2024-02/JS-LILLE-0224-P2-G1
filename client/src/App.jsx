4dd5cc6b1e53edde0ef4dd44e75ebef28d655ed1
VINCENT VAN VOLSEM <vincent.vanvolsem@gmail.com>
2024-04-12 11:43:27 +0200

    replacement .fa-icon with .fa_icon

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,11 +2,11 @@ import "./App.css";
 import Footer from "./components/Footer";
 
 function App() {
-  return <div>
-    <Footer />
-  </div>;
-
-
+  return (
+    <div>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
