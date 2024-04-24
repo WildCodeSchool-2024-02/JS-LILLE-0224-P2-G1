521c6103b497be82c3ba5b4949387b2bf45825fe
BENOITMEZAGUER <161327017+BENOITMEZAGUER@users.noreply.github.com>
2024-04-24 11:39:37 +0200

    Update NotFound.jsx
    rectification with camelCase

--- client/src/components/NotFound/NotFound.jsx
+++ client/src/components/NotFound/NotFound.jsx
@@ -3,21 +3,21 @@ import "./NotFound.css";
 function NotFound() {
   //
   return (
-    <div className="NotFound">
+    <div className="notFound">
       {/* Conteneur de la page d'erreur */}
       <h2 className="Not_Found_Title"> Error 404 - Not Found</h2>
       {/* Titre de la page d'erreur */}
       <img
         src="https://media.istockphoto.com/id/1397562936/vector/cat-drinks-beer.jpg?s=612x612&w=0&k=20&c=Q9cd6sxayXMaUGegwFjsLoSkldlZlu_0k8xXJybBqbE="
         alt="AperoCat"
-        className="Not_Found_Img"
+        className="not_Found_Img"
       />
       {/* Image de la page d'erreur */}
-      <p className="Not_Found_Text">
+      <p className="not_Found_Text">
         Are meow lost? Maybe meow should go there!{" "}
       </p>
       {/* texte sous l'image */}
-      <a className="Home_Page_Link" href="/">
+      <a className="home_Page_Link" href="/">
         {" "}
         Homepage
       </a>
