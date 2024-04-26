e5067e9d8e8f582e0ae367179e8b2f12574ffc11
BENOITMEZAGUER <benoit.mezaguer@gmail.com>
2024-04-26 11:21:57 +0200

    hotfix on the jsx

--- client/src/components/NotFound/NotFound.jsx
+++ client/src/components/NotFound/NotFound.jsx
@@ -1,11 +1,10 @@
 import "./NotFound.scss";
 
 function NotFound() {
-
   return (
     <div className="notFound">
       {/* Error page container */}
-      <h2 className="Not_Found_Title"> Error 404 - Not Found</h2>
+      <h2 className="not_Found_Title"> Error 404 - Not Found</h2>
       {/* Error page title */}
       <img
         src="https://media.istockphoto.com/id/1397562936/vector/cat-drinks-beer.jpg?s=612x612&w=0&k=20&c=Q9cd6sxayXMaUGegwFjsLoSkldlZlu_0k8xXJybBqbE="
